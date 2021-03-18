23aa061230c85d925a1f93352a97b7688ecac7be
suyeon1218 <msjgr2801@gmail.com>
2021-03-18 16:17:12 +0900

    20210309수연.py

--- suyeon/20210309수연.py
+++ suyeon/20210309수연.py
@@ -1,10 +1,11 @@
 // https://www.acmicpc.net/problem/2292
 
 N=int(input("N값: "))
-count=1;
-room=1;
+count=1; #
+room=1; #거치는 방의 수
 
-value=int(N/6)
+value=int(N/6) 
+#6각형이 군집된 형태라 일차적으로 6으로 나눔
 
 if(N==1):
     print("1")
@@ -12,8 +13,8 @@ if(N==1):
     
 
 while(value>count):
-    room+=1;
-    count=count+room
+    room+=1; #방을 하나씩 더 거침
+    count=count+room #방을 거칠때 증가하는 피보나치
     
 
 if(N==count*6+1):
