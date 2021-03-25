86d383469afe1d198c6f737caaf211489c251061
unknown <KimJongBo>
2021-03-25 16:03:37 +0900

    20210325 KimJongBo

---
+++ KimJongBo/210325_9012.py
@@ -0,0 +1,39 @@
+class Stack(list):
+    push = list.append # push
+    pop = list.pop # pop
+    
+    def is_empty(self):
+        if not self:
+            return True
+        else:
+            return False #스택 클래스
+
+def stackCNT(string,lengthg):
+    stack = []
+    for i in string:
+        op = '('
+        cl = ')'
+        if i in op: # push('('로 시작될때 스택에  넣기)
+            stack.append(i)            
+            if op.find(stack.pop()) != cl.find(i):
+                return True #pop (,) 다르면 True
+            
+            
+        if i in cl: # push( ')'로 시작될때 스택에  넣기)
+            stack.append(i)
+            if cl.find(stack.pop()) != op.find(i):
+                return False #pop ),( 다르면 False
+            
+    if not stack:
+        return True #스택이 비어있다면  Ture
+    else:
+        return False #스택에 남아 있다면 False 
+
+while True:
+    string = str(input("input ()  : "))
+    lenstring = len(string)
+    print(lenstring)
+    if stackCNT(string,lenstring) == False:
+        print('NO')
+    else:
+        print('YES')
